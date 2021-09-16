b46f4cbade0429c61e4b631ecc62705411b8e539
sudipto-g <85854843+sudipto-g@users.noreply.github.com>
2021-09-16 11:22:59 +0530

    Initial commit

---
+++ data/IIEST_Landmarks_2.js
@@ -0,0 +1,29 @@
+var json_IIEST_Landmarks_2 = {
+"type": "FeatureCollection",
+"name": "IIEST_Landmarks_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Name": "Hostel 8" }, "geometry": { "type": "Point", "coordinates": [ 88.302492779913209, 22.557157650291373 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Name": "Hostel 10" }, "geometry": { "type": "Point", "coordinates": [ 88.302944975445087, 22.556652131931454 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Name": "Hostel 11" }, "geometry": { "type": "Point", "coordinates": [ 88.301929518812116, 22.556498278149842 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Name": "Downing Hall" }, "geometry": { "type": "Point", "coordinates": [ 88.301707387673645, 22.555084280597381 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Name": "Williams" }, "geometry": { "type": "Point", "coordinates": [ 88.302770443836295, 22.554688651744193 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Name": "Nivedita Hall" }, "geometry": { "type": "Point", "coordinates": [ 88.304015964862671, 22.556784006464831 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Name": "Wolfenden Hall" }, "geometry": { "type": "Point", "coordinates": [ 88.304785490592323, 22.557194281984593 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Name": "Pandya Hall" }, "geometry": { "type": "Point", "coordinates": [ 88.306150010442877, 22.556945186278842 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Name": "Guest House" }, "geometry": { "type": "Point", "coordinates": [ 88.306435607620898, 22.556446993517834 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Name": "Model School" }, "geometry": { "type": "Point", "coordinates": [ 88.304444360629702, 22.554007288281792 ] } },
+{ "type": "Feature", "properties": { "id": "11", "Name": "UCO Bank" }, "geometry": { "type": "Point", "coordinates": [ 88.306229342992324, 22.553882737607658 ] } },
+{ "type": "Feature", "properties": { "id": "12", "Name": "Ramanujan C Library" }, "geometry": { "type": "Point", "coordinates": [ 88.308974249203303, 22.554864486930132 ] } },
+{ "type": "Feature", "properties": { "id": "13", "Name": "Hostel 14" }, "geometry": { "type": "Point", "coordinates": [ 88.309236046616505, 22.555670395331294 ] } },
+{ "type": "Feature", "properties": { "id": "14", "Name": "Hostel 15" }, "geometry": { "type": "Point", "coordinates": [ 88.309759641442881, 22.555582478279963 ] } },
+{ "type": "Feature", "properties": { "id": "15", "Name": "Sengupta Hall" }, "geometry": { "type": "Point", "coordinates": [ 88.310362568818704, 22.556036715775377 ] } },
+{ "type": "Feature", "properties": { "id": "16", "Name": "McDonald Hostel" }, "geometry": { "type": "Point", "coordinates": [ 88.309251913126403, 22.556622826462476 ] } },
+{ "type": "Feature", "properties": { "id": "17", "Name": "Richardson Hall" }, "geometry": { "type": "Point", "coordinates": [ 88.308315789042879, 22.556893901812995 ] } },
+{ "type": "Feature", "properties": { "id": "18", "Name": "Lords Ground" }, "geometry": { "type": "Point", "coordinates": [ 88.308395121592326, 22.55613928532539 ] } },
+{ "type": "Feature", "properties": { "id": "19", "Name": "Oval Ground" }, "geometry": { "type": "Point", "coordinates": [ 88.304587159218698, 22.555956125361142 ] } },
+{ "type": "Feature", "properties": { "id": "20", "Name": "ITI College" }, "geometry": { "type": "Point", "coordinates": [ 88.306221409737375, 22.554446877997574 ] } },
+{ "type": "Feature", "properties": { "id": "21", "Name": "IIEST Hospital" }, "geometry": { "type": "Point", "coordinates": [ 88.305372551458262, 22.557121018588422 ] } },
+{ "type": "Feature", "properties": { "id": "22", "Name": "Main Building" }, "geometry": { "type": "Point", "coordinates": [ 88.304349161570343, 22.555150218629265 ] } }
+]
+}
